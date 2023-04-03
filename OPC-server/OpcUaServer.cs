35a34414fbe535c0075c2d8434f2279bf4e6f8f2
bingyu <xyz50270@gmail.com>
2023-04-03 21:19:46 +0800

    添加项目文件。

---
+++ OPC-server/OpcUaServer.cs
@@ -0,0 +1,68 @@
+﻿using Opc.Ua.Server;
+using Opc.Ua;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Opc.Ua.Configuration;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace OPC_server
+{
+    public class MyOpcUaServer : StandardServer
+    {
+        private ConnectionMultiplexer redisConnection;
+        private IDatabase redisDatabase;
+
+        protected override MasterNodeManager CreateMasterNodeManager(IServerInternal server, ApplicationConfiguration configuration)
+        {
+            // Connect to Redis
+            redisConnection = ConnectionMultiplexer.Connect("localhost");
+            redisDatabase = redisConnection.GetDatabase();
+
+            // Create the custom node manager
+            var nodeManagers = new INodeManager[] { new MyNodeManager(server, configuration, redisDatabase) };
+            return new MasterNodeManager(server, configuration, null, nodeManagers);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (redisConnection != null)
+            {
+                redisConnection.Dispose();
+                redisConnection = null;
+            }
+            base.Dispose(disposing);
+        }
+    }
+
+    public class MyNodeManager : CustomNodeManager2
+    {
+        private IDatabase redisDatabase;
+
+        public MyNodeManager(IServerInternal server, ApplicationConfiguration configuration, IDatabase redisDatabase)
+            : base(server, configuration, Namespaces.OpcUa)
+        {
+            this.redisDatabase = redisDatabase;
+
+            //SystemContext.NodeIdFactory.SetNamespaceIndex(1);
+
+            // Load the initial data from Redis and create the corresponding OPC UA nodes
+            LoadDataFromRedis();
+        }
+
+        private void LoadDataFromRedis()
+        {
+            // Load data from Redis and create OPC UA nodes
+        }
+
+        // Implement the methods to handle data changes in Redis, and update the OPC UA nodes accordingly
+    }
+
+    internal class OpcUaServer
+    {
+
+    }
+}
---
+++ OPC-server/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting.Server;
+using Opc.Ua;
+using Opc.Ua.Configuration;
+using Opc.Ua.Server;
+using StackExchange.Redis;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace OPC_server
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            CertificatePasswordProvider PasswordProvider = new CertificatePasswordProvider("");
+            ApplicationInstance application = new ApplicationInstance
+            {
+                ApplicationName = "MyOpcUaServer",
+                ApplicationType = ApplicationType.Server,
+                ConfigSectionName = "MyOpcUaServer",
+                CertificatePasswordProvider = PasswordProvider
+            };
+
+
+            // load the application configuration.
+            await application.LoadApplicationConfiguration(false).ConfigureAwait(false);
+
+            // Load the application configuration.
+            ApplicationConfiguration config = await application.LoadApplicationConfiguration(false).ConfigureAwait(false);
+
+            // Check the application certificate.
+            bool haveAppCertificate = await application.CheckApplicationInstanceCertificate(false, 0);
+
+            if (!haveAppCertificate)
+            {
+                throw new Exception("Application instance certificate invalid!");
+            }
+
+            // Start the server.
+            await application.Start(new MyOpcUaServer());
+            Console.WriteLine("Server started. Press Enter to exit...");
+            Console.ReadLine();
+
+            // Stop the server before exiting.
+            application.Stop();
+
+        }
+    }
+}
